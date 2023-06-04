bf04835f1a875675d48aa7eb392e7770ad0df4a4
okara533 <ozankara@hotmail.com>
2023-06-04 15:37:15 +0300

    adding sound effect

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 
 let cardDiv=document.querySelectorAll(".card")
-
-
+var audio = new Audio('asset/correct.wav');
+let greenAudio=new Audio("asset/g.mp3")
+let yellowAudio=new Audio("asset/y.mp3")
+let redAudio=new Audio("asset/r.mp3")
+let purpleAudio=new Audio("asset/p.mp3")
 for(let i=0;i<4;i++){
     cardDiv[i].addEventListener("click",()=>{
         
         cardDiv[i].classList.add("clicked");
+        switch (cardDiv[i].classList[0]){
+            case "green":
+                greenAudio.currentTime = 0;
+                greenAudio.play();
+                break;
+            case "red":
+                redAudio.currentTime = 0;
+                redAudio.play();
+                break;
+            case "yellow":
+                yellowAudio.currentTime = 0;
+                yellowAudio.play();
+                break;
+            case "purple":
+                purpleAudio.currentTime = 0;
+                purpleAudio.play();
+                break;
+        }
+        
         setTimeout(() => {
             cardDiv[i].classList.remove("clicked");
+
           }, "50");
         
           
